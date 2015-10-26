a2e7c99e642a9e94ef9a62f07734ee00bea57436
dragonjet <jetriconew@gmail.com>
2015-10-26 16:54:42 +0800

    Dynamic Assets

---
+++ scripts/kcloader.js
@@ -0,0 +1,22 @@
+(function(){
+	
+	var ServerNum = parseInt( window.location.hash.substring(1) ,10) || 1;
+	var ServerIndex = ServerNum - 1;
+	var ServerData = servers[ServerIndex];
+	var ServerIP = ServerData.ip;
+	
+	$(document).on("ready", function(){
+		$("#progress-title").text(ServerData.name);
+		
+		$.each(statics, function(index, StaticAsset){
+			$("#embeds").append("<embed width=\"0%\" height=\"0%\" name=\"plugin\" src=\"http://"+ServerIP+"/"+StaticAsset+"\" type=\"application/x-shockwave-flash\">");
+		});
+		
+		$.each(shipgraph, function(index, ShipGraph){
+			// console.log( ShipGraph.api_filename + "?version=" + ShipGraph.api_version);
+			// $("#embeds").append("<embed src=\"http://"+ServerIP+"/"+StaticAsset+"\">");
+		});
+		
+	});
+	
+})();
