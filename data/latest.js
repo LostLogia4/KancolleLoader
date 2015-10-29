ea93229ec77f96d75f3e4fe159857a027e4f1fed
LostLogia4 <hayate04@live.com>
2015-10-29 16:01:38 +0800

    latest.js test finished

--- data/latest.js
+++ data/latest.js
@@ -3,23 +3,26 @@ var latest = [
 	//insert new stuffs here
 	/*
 	"/kcs/resources/swf/ships/zuikaku-kai_ni.swf",
-	"/kcs/resources/swf/ships/zuikaku-kai_ni_A.swf",
 	*/
 //	last resource has no comma at its end.
-//	"/kcs/resources/swf/map/31_06.swf?version=1.0.0"
-
-	"/kcs/resources/swf/ships/wvnchfzhekfo.swf"
+//	"/kcs/resources/swf/ships/zuikaku-kai_ni_A.swf"
 
 ];
 
-	//*Prototype for dedicated loader.
+	
+	/*
+	//*Standalone loader.
 	$(document).on("ready", function(){
 		
+		var ServerNum = parseInt( window.location.hash.substring(1) ,10) || 1;
+		var ServerIndex = ServerNum - 1;
+		var ServerData = servers[ServerIndex];
+		var ServerIP = ServerData.ip;
+
 		//Load the data values
 		$.each(latest, function(index, StaticAsset){
 			$("#embeds").append("<embed width=\"0%\" height=\"0%\" style=\"visibility:hidden;\" src=\"http://"+ServerIP+"/"+StaticAsset+"\" type=\"application/x-shockwave-flash\">");
 		});
 		
 	});
-	//*/
-	
+	/*
