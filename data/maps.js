707655cdcb6fe90a79553df287b0711a9aaf8202
LostLogia4 <hayate04@live.com>
2016-01-19 18:07:32 +0800

    Shortening links

--- data/maps.js
+++ data/maps.js
@@ -1,43 +1,41 @@
 var worldmaps = [
-
-//eventmaps
-/*
-"kcs/resources/swf/map/33_01.swf?version=1.0.0",
-"kcs/resources/swf/map/33_02.swf?version=1.0.0",
-"kcs/resources/swf/map/33_03.swf?version=1.0.0",
-"kcs/resources/swf/map/33_04.swf?version=1.0.0",
-"kcs/resources/swf/map/33_05.swf?version=1.0.0",
-"kcs/resources/swf/map/33_06.swf?version=1.0.0",
-*/
-
-//world maps
-"kcs/resources/swf/map/01_01.swf?version=1.0.0",
-"kcs/resources/swf/map/01_02.swf?version=1.0.0",
-"kcs/resources/swf/map/01_03.swf?version=1.0.0",
-"kcs/resources/swf/map/01_04.swf?version=1.0.0",
-"kcs/resources/swf/map/01_05.swf?version=1.0.0",
-"kcs/resources/swf/map/01_06.swf?version=1.0.0",
-"kcs/resources/swf/map/02_01.swf?version=1.0.0",
-"kcs/resources/swf/map/02_02.swf?version=1.0.0",
-"kcs/resources/swf/map/02_03.swf?version=1.0.0",
-"kcs/resources/swf/map/02_04.swf?version=1.0.0",
-"kcs/resources/swf/map/02_05.swf?version=1.0.0",
-"kcs/resources/swf/map/03_01.swf?version=1.0.0",
-"kcs/resources/swf/map/03_02.swf?version=1.0.0",
-"kcs/resources/swf/map/03_03.swf?version=1.0.0",
-"kcs/resources/swf/map/03_04.swf?version=1.0.0",
-"kcs/resources/swf/map/03_05.swf?version=1.0.0",
-"kcs/resources/swf/map/04_01.swf?version=1.0.0",
-"kcs/resources/swf/map/04_02.swf?version=1.0.0",
-"kcs/resources/swf/map/04_03.swf?version=1.0.0",
-"kcs/resources/swf/map/04_04.swf?version=1.0.0",
-"kcs/resources/swf/map/04_05.swf?version=1.0.0",
-"kcs/resources/swf/map/05_01.swf?version=1.0.0",
-"kcs/resources/swf/map/05_02.swf?version=1.0.0",
-"kcs/resources/swf/map/05_03.swf?version=1.0.0",
-"kcs/resources/swf/map/05_04.swf?version=1.0.0",
-"kcs/resources/swf/map/05_05.swf?version=1.0.0",
-"kcs/resources/swf/map/06_01.swf?version=1.0.0",
-"kcs/resources/swf/map/06_02.swf?version=1.0.0",
-"kcs/resources/swf/map/06_03.swf?version=1.0.0"
+	
+	//eventmaps
+	/*
+	"33_01.swf?version=1.0.0",
+	"33_02.swf?version=1.0.0",
+	"33_03.swf?version=1.0.0",
+	"33_04.swf?version=1.0.0",
+	*/
+	
+	//world maps
+	"01_01.swf?version=1.0.0",
+	"01_02.swf?version=1.0.0",
+	"01_03.swf?version=1.0.0",
+	"01_04.swf?version=1.0.0",
+	"01_05.swf?version=1.0.0",
+	"01_06.swf?version=1.0.0",
+	"02_01.swf?version=1.0.0",
+	"02_02.swf?version=1.0.0",
+	"02_03.swf?version=1.0.0",
+	"02_04.swf?version=1.0.0",
+	"02_05.swf?version=1.0.0",
+	"03_01.swf?version=1.0.0",
+	"03_02.swf?version=1.0.0",
+	"03_03.swf?version=1.0.0",
+	"03_04.swf?version=1.0.0",
+	"03_05.swf?version=1.0.0",
+	"04_01.swf?version=1.0.0",
+	"04_02.swf?version=1.0.0",
+	"04_03.swf?version=1.0.0",
+	"04_04.swf?version=1.0.0",
+	"04_05.swf?version=1.0.0",
+	"05_01.swf?version=1.0.0",
+	"05_02.swf?version=1.0.0",
+	"05_03.swf?version=1.0.0",
+	"05_04.swf?version=1.0.0",
+	"05_05.swf?version=1.0.0",
+	"06_01.swf?version=1.0.0",
+	"06_02.swf?version=1.0.0",
+	"06_03.swf?version=1.0.0"
 ];
