2d93151ab3339bb55d3ae2cd76a6aa8d00dc245c
LostLogia4 <hayate04@live.com>
2016-01-20 21:10:13 +0800

    interface.js

---
+++ data/interface.js
@@ -0,0 +1,54 @@
+//generated link
+//$("#embeds").append(imglink+ServerIP+"uilinkage[index]"+"?version="+uiversion[index]+"\">");
+
+var uilinkage = [
+		"mm_common":"/kcs/resources/swf/commonAssets.swf",
+		"mm_port":"/kcs/PortMain.swf",
+		"rm_font":"/kcs/resources/swf/font.swf",
+		"rm_icon":"/kcs/resources/swf/icons.swf",
+		"rm_sound_bgm":"/kcs/resources/swf/sound_bgm.swf",
+		"rm_sound_se":"/kcs/resources/swf/sound_se.swf",
+		"rm_itemicon":"/kcs/resources/swf/itemicons.swf",
+		"rm_stype":"/kcs/resources/swf/stype.swf",
+		"um_record":"/kcs/scenes/RecordMain.swf",
+		"um_album":"/kcs/scenes/AlbumMain.swf",
+		"um_itemlist":"/kcs/scenes/ItemlistMain.swf",
+		"um_interior":"/kcs/scenes/InteriorMain.swf",
+		"um_duty":"/kcs/scenes/DutyMain.swf",
+		"lm_organize":"/kcs/scenes/OrganizeMain.swf",
+		"lm_supply":"/kcs/scenes/SupplyMain.swf",
+		"lm_remodel":"/kcs/scenes/RemodelMain.swf",
+		"lm_repair":"/kcs/scenes/RemodelMain.swf",
+		"lm_arsenal":"/kcs/scenes/ArsenalMain.swf",
+		"lm_sally":"/kcs/scenes/SallyMain.swf",
+		"mm_battle":"/kcs/scenes/BattleMain.swf",
+		"mm_battleresult":"/kcs/scenes/BattleMain.swf",
+		"xm_wedding":"/kcs/scenes/WeddingMain.swf",
+		"xm_revamp":"/kcs/scenes/RevampMain.swf"
+		];
+
+var uiversion = [
+		"mm_common":"2.6.8",
+		"mm_port":"2.6.6",
+		"rm_font":"2.3",
+		"rm_icon":"2.5",
+		"rm_sound_bgm":"2.1.7",
+		"rm_sound_se":"2.6.4",
+		"rm_itemicon":"2.6.9",
+		"rm_stype":"2.6.4",
+		"um_record":"2.3.8",
+		"um_album":"2.6.6.1",
+		"um_itemlist":"2.6.12",
+		"um_interior":"2.3",
+		"um_duty":"2.6.12",
+		"lm_organize":"2.6.10",
+		"lm_supply":"2.6.5",
+		"lm_remodel":"2.6.12",
+		"lm_repair":"2.3.5",
+		"lm_arsenal":"2.6.0",
+		"lm_sally":"2.6.7",
+		"mm_battle":"2.6.6",
+		"mm_battleresult":"2.6.9",
+		"xm_wedding":"2.0.10",
+		"xm_revamp":"2.6.4"
+		];
