0f4587b8dd6cf436bfc74407dc0868bf228b1ad9
LostLogia4 <hayate04@live.com>
2016-01-22 11:06:02 +0800

    Update pseudocode

--- scripts/kcsload.js
+++ scripts/kcsload.js
@@ -3,10 +3,21 @@
 	The loading button will parse the checkboxes and assign a local boolean variable for each checkboxes
 	and execute the associated functions.
 	
+	functiondisable(){
+		//1. if past expiry date, disable dynamic asset checkboxes
+		//shipCG, funitureCG, interface, voiceline
+	}
+
 	functionloadclick(){
 		//1. parse checkboxes
 		//2. assign boolean values
-		//2. if true execute
+		//3. if true execute
+	}
+
+	functionbypass(){
+		//1. create a confirm box.
+		//ref (http://www.w3schools.com/js/tryit.asp?filename=tryjs_confirm)
+		//2. if yes, remove the alert box and reenable dynamic asset checkboxes.
 	}
 	
 */
