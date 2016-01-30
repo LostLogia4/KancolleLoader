bb5475c06074e91f1cf17f5ee69d7b6d6e104a35
LostLogia4 <hayate04@live.com>
2016-01-30 13:41:08 +0800

    Updated expiryDate variable to 10th Feb

--- data/maintenance.js
+++ data/maintenance.js
@@ -1,5 +1,5 @@
 //value for next Maintenance Date
-var expiryDate = "08 February 2016 11:00 GMT+0900";
+var expiryDate = "10 February 2016 11:00 GMT+0900";
 
 //list of blacklisted api_shipgraph IDs which contains 404 links.
 //api_id values are integers, hence no quotes.
