8bbf0d5cd3583bf74d5073f1ec547957f3e22d6e
LostLogia4 <hayate04@live.com>
2016-03-21 09:31:30 +0800

    Update item.js

--- data/item.js
+++ data/item.js
@@ -1,5 +1,5 @@
 //edit here when new equipment are introduced
-var CurrentMaxEquip = 165;
+var CurrentMaxEquip = 167;
 
 var UseItem = [ "004", "052", "054", "055", "056", "057", "058", "059", "060", "061", "062", "063", "064", "065", "066", "067", "068", "069", "070", "071", "072" ];
 
