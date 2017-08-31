7e404af93d70253281ccc6733837047feaec6f3a
LostLogia4 <hayate04@live.com>
2017-08-31 18:03:21 +0800

    Update maintenance.js

--- data/maintenance.js
+++ data/maintenance.js
@@ -1,7 +1,7 @@
 //value for next Maintenance Date
-var expiryDate = "7 August 2017 11:00 GMT+0900";
+var expiryDate = "11 September 2017 11:00 GMT+0900";
 
 //list of blacklisted api_shipgraph IDs which contains 404 links.
 //api_id values are integers, hence no quotes.
-var blacklistID = [ 563, 568 , 569 , 580 , 593 , 596 ];
+var blacklistID = [ /*563, 568 , 569 , 580 , 593 , 596*/ ];
 
